64d9ce903db88c368d5b8732c7b66e1ca8317f66
OkiAzama <62458273+OkiAzama@users.noreply.github.com>
2020-03-21 15:18:38 +0900

    Add files via upload
    3rd up;oad

---
+++ common/js/carousel/script.js
@@ -0,0 +1,11 @@
+$(function () {
+	//カルーセルの設定
+	$('.gimmicCarousel_sample .slider').slick({
+		accessibility: true,
+		dots: true,
+		autoplay: true,
+		autoplaySpeed:3000,
+		infinite: true,
+		speed: 500
+	});
+});
